c19ff8ba534151a6f554bd993d843d616a795a26
Jose Perez <joseperu2503@gmail.com>
2025-01-29 23:00:46 -0500

    external layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,20 @@ import { Routes } from '@angular/router';
 import { guestGuard } from './core/guards/guest/guest.guard';
 import { authGuard } from './core/guards/auth/auth.guard';
 import { ChatsLayoutComponent } from './features/chats/components/chats-layout/chats-layout.component';
+import { ExternalLayoutComponent } from './features/chats/components/external-layout/external-layout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./features/chats/pages/chat-page/chat-page.component'),
-    pathMatch: 'full',
+    component: ExternalLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/chats/pages/chat-page/chat-page.component'),
+        pathMatch: 'full',
+      },
+    ],
   },
   {
     path: '',
---
+++ src/app/features/chats/components/external-layout/external-layout.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-external-layout',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './external-layout.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ExternalLayoutComponent {
+  private route = inject(Router);
+  onSignIn() {
+    this.route.navigate(['/login']);
+  }
+}
